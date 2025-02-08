1afc5fda812b10aa81195112c1134ea7983df0d8
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-08 14:33:40 +0100

    test

--- Sources/Coenttb Identity Consumer/Client/Identity Consumer Client.live.swift
+++ Sources/Coenttb Identity Consumer/Client/Identity Consumer Client.live.swift
@@ -10,10 +10,11 @@ import RateLimiter
 extension Identity.Consumer.Client {
     public static func live(
         provider: Identity.Consumer.Client.Live.Provider,
+        router: AnyParserPrinter<URLRequestData, Identity.Consumer.API>,
         makeRequest: (AnyParserPrinter<URLRequestData, Identity.Consumer.API>) -> (_ route: Identity.Consumer.API) throws -> URLRequest = Identity.Consumer.Client.Live.makeRequest
     ) -> Self {
         
-        @Dependency(Identity.API.Router.self) var router
+//        @Dependency(Identity.API.Router.self) var router
         
         let apiRouter = router.baseURL(provider.baseURL.absoluteString).eraseToAnyParserPrinter()
         
