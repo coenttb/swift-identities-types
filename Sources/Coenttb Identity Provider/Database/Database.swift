138c327216fc27243265f1e84aa88e2fd0ed7d9c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-08 10:02:51 +0100

    wip

--- Sources/Coenttb Identity Provider/Database/Database.swift
+++ Sources/Coenttb Identity Provider/Database/Database.swift
@@ -6,5 +6,52 @@
 //
 
 import Foundation
+import Fluent
 
-package enum Database {}
+public enum Database {}
+
+extension Database {
+    public struct Migration: AsyncMigration {
+        
+        public var name: String = "Coenttb_Identity_Provider.Database.Migration.Create"
+        
+        private static let migrations: [any Fluent.Migration] = {
+            var migrations: [any Fluent.Migration] = [
+                {
+                    var migration = Coenttb_Identity_Provider.Database.Identity.Migration.Create()
+                    migration.name = "Coenttb_Identity.Database.Identity.Migration.Create"
+                    return migration
+                }(),
+                {
+                    var migration = Coenttb_Identity_Provider.Database.Identity.Token.Migration()
+                    migration.name = "Coenttb_Identity_Provider.Database.Identity.Token.Migration.Create"
+                    return migration
+                }(),
+                {
+                    var migration = Coenttb_Identity_Provider.Database.EmailChangeRequest.Migration()
+                    migration.name = "Coenttb_Identity.Database.EmailChangeRequest.Migration.Create"
+                    return migration
+                }(),
+                {
+                    var migration = Coenttb_Identity_Provider.Database.ApiKey.Migration.Create()
+                    migration.name = "Coenttb_Identity.Database.ApiKey.Migration.Create"
+                    return migration
+                }()
+            ]
+            
+            return migrations
+        }()
+        
+        public func prepare(on database: Fluent.Database) async throws {
+            for migration in Self.migrations {
+                let _ = migration.prepare(on: database)
+            }
+        }
+        
+        public func revert(on database: Fluent.Database) async throws {
+            for migration in Self.migrations {
+                let _ = migration.revert(on: database)
+            }
+        }
+    }
+}
