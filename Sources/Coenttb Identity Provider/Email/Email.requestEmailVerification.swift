222f0c8c501f6df0ed9f56e5c485bf674baba439
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-09 09:43:07 +0100

    wip

--- Sources/Coenttb Identity Provider/Email/Email.requestEmailVerification.swift
+++ Sources/Coenttb Identity Provider/Email/Email.requestEmailVerification.swift
@@ -20,77 +20,75 @@ extension Email {
         primaryColor: HTMLColor
     ) -> Self {
         
-        let html = TableEmailDocument(
-            preheader: TranslatedString(
-                dutch: "Verifiëer je emailadres voor \(businessName)",
-                english: "Verify your email for \(businessName)"
-            ).description
-        ) {
-            tr {
-                td {
-                    VStack(alignment: .leading) {
-                        Header(3) {
-                            TranslatedString(
-                                dutch: "Verifiëer je emailadres",
-                                english: "Verify your email address"
-                            )
-                        }
-                        
-                        Paragraph {
-                            TranslatedString(
-                                dutch: "Om de setup van je \(businessName) account te voltooien, bevestig alsjeblieft dat dit je e-mailadres is.",
-                                english: "To continue setting up your \(businessName) account, please verify that this is your email address."
-                            )
-                        }
-                        .padding(bottom: .extraSmall)
-                        .fontSize(.body)
-                        
-                        Button(
-                            tag: a,
-                            background: primaryColor
-                        ) {
-                            TranslatedString(
-                                dutch: "Verifieer e-mailadres",
-                                english: "Verify email address"
-                            )
-                        }
-                        .color(.primary.reverse())
-                        .href(verificationUrl.absoluteString)
-                        .padding(bottom: Length.medium)
-                        
-                        
-                        Paragraph(.small) {
-                            
-                            TranslatedString(
-                                dutch: "Om veiligheidsredenen verloopt deze verificatielink binnen 24 uur. ",
-                                english: "This verification link will expire in 24 hours for security reasons. "
-                            )
-                            
-                            TranslatedString(
-                                dutch: "Als je deze aanvraag niet hebt gedaan, kun je deze e-mail negeren.",
-                                english: "If you did not make this request, please disregard this email."
-                            )
-                            
-                            br()
-                            
-                            TranslatedString(
-                                dutch: "Voor hulp, neem contact op met ons op via \(supportEmail).",
-                                english: "For help, contact us at \(supportEmail)."
-                            )
-                        }
-                        .fontSize(.footnote)
-                        .color(.secondary)
-                    }
-                    .padding(vertical: .small, horizontal: .medium)
-                }
-            }
-        }
-            .backgroundColor(.primary.reverse())
-            
+//        let html = TableEmailDocument(
+//            preheader: TranslatedString(
+//                dutch: "Verifiëer je emailadres voor \(businessName)",
+//                english: "Verify your email for \(businessName)"
+//            ).description
+//        ) {
+//            tr {
+//                td {
+//                    VStack(alignment: .leading) {
+//                        Header(3) {
+//                            TranslatedString(
+//                                dutch: "Verifiëer je emailadres",
+//                                english: "Verify your email address"
+//                            )
+//                        }
+//                        
+//                        Paragraph {
+//                            TranslatedString(
+//                                dutch: "Om de setup van je \(businessName) account te voltooien, bevestig alsjeblieft dat dit je e-mailadres is.",
+//                                english: "To continue setting up your \(businessName) account, please verify that this is your email address."
+//                            )
+//                        }
+//                        .padding(bottom: .extraSmall)
+//                        .fontSize(.body)
+//                        
+//                        Button(
+//                            tag: a,
+//                            background: primaryColor
+//                        ) {
+//                            TranslatedString(
+//                                dutch: "Verifieer e-mailadres",
+//                                english: "Verify email address"
+//                            )
+//                        }
+//                        .color(.primary.reverse())
+//                        .href(verificationUrl.absoluteString)
+//                        .padding(bottom: Length.medium)
+//                        
+//                        
+//                        Paragraph(.small) {
+//                            
+//                            TranslatedString(
+//                                dutch: "Om veiligheidsredenen verloopt deze verificatielink binnen 24 uur. ",
+//                                english: "This verification link will expire in 24 hours for security reasons. "
+//                            )
+//                            
+//                            TranslatedString(
+//                                dutch: "Als je deze aanvraag niet hebt gedaan, kun je deze e-mail negeren.",
+//                                english: "If you did not make this request, please disregard this email."
+//                            )
+//                            
+//                            br()
+//                            
+//                            TranslatedString(
+//                                dutch: "Voor hulp, neem contact op met ons op via \(supportEmail).",
+//                                english: "For help, contact us at \(supportEmail)."
+//                            )
+//                        }
+//                        .fontSize(.footnote)
+//                        .color(.secondary)
+//                    }
+//                    .padding(vertical: .small, horizontal: .medium)
+//                }
+//            }
+//        }
+//            .backgroundColor(.primary.reverse())
+//            .render()
+
         
-        
-        let bytes: ContiguousArray<UInt8> = html.render()
-        let string: String = String(decoding: bytes, as: UTF8.self)
         
         let subjectAdd = TranslatedString(
             dutch: "Verifieer je e-mailadres",
@@ -101,8 +99,8 @@ extension Email {
             from: from,
             to: [ user.email ],
             subject: "\(businessName) | \(subjectAdd)",
-            html: string,
-            text: nil
+            html: /*String(decoding: html, as: UTF8.self)*/nil,
+            text: "test email text"
         )
     }
 }
