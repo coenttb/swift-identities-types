41a2f2742ab52480ec456bc756f1499ea24851c6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-11 10:47:02 +0100

    test

--- Sources/Coenttb Identity Consumer/HTTPCookies.Value.swift
+++ Sources/Coenttb Identity Consumer/HTTPCookies.Value.swift
@@ -59,7 +59,7 @@ extension HTTPCookies.Value {
         return .jwt(
             token: response.refreshToken.value,
             expiresIn: response.refreshToken.expiresIn,
-            path: router.url(for: .api(.authenticate(.token(.refresh(.init(token: "")))))).relativePath,
+            path: router.url(for: .api(.authenticate(.token(.refresh(.init(token: "----------------")))))).relativePath,
             domain: domain,
             isSecure: {
 #if DEBUG
