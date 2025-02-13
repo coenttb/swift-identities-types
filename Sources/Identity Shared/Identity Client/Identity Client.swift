f7e22cd615c3b08b941e29e99d2f46b753ae3dd0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-02-13 12:14:32 +0100

    wip

--- Sources/Identity Shared/Identity Client/Identity Client.swift
+++ Sources/Identity Shared/Identity Client/Identity Client.swift
@@ -29,8 +29,6 @@ extension Identity {
         
         public var password: Identity.Client.Password        
         
-        
-        
         public init(
             authenticate: Identity.Client.Authenticate,
             logout: @escaping () async throws -> Void,
@@ -50,13 +48,53 @@ extension Identity {
 }
 
 
+extension Identity.Client.Password.Reset {
+    public func request(_ request: Identity.Password.Reset.Request) async throws {
+        try await self.request(email: try .init(request.email))
+    }
+}
 
+extension Identity.Client.Password.Reset {
+    public func confirm(_ confirm: Identity.Password.Reset.Confirm) async throws {
+        try await self.confirm(newPassword: confirm.newPassword, token: confirm.token)
+    }
+}
 
+extension Identity.Client.Password.Change {
+    public func request(_ request: Identity.Password.Change.Request) async throws {
+        try await self.request(
+            currentPassword: request.currentPassword,
+            newPassword: request.newPassword
+        )
+    }
+}
 
+extension Identity.Client.Create {
+    public func request(_ request: Identity.Create.Request) async throws {
+        try await self.request(email: try .init(request.email), password: request.password)
+    }
+}
 
+extension Identity.Client.Create {
+    public func verify(_ verify: Identity.Create.Verify) async throws {
+        try await self.verify(email: try .init(verify.email), token: verify.token)
+    }
+}
 
+extension Identity.Client.EmailChange {
+    public func request(_ request: Identity.EmailChange.Request) async throws -> Identity.Client.EmailChange.Request.Result {
+        return try await self.request(newEmail: try .init(request.newEmail))
+    }
+}
 
+extension Identity.Client.EmailChange {
+    public func confirm(_ confirm: Identity.EmailChange.Confirm) async throws {
+        try await self.confirm(token: confirm.token)
+    }
+}
 
-
-
-
+extension Identity.Client.Delete {
+    public func request(_ request: Identity.Delete.Request) async throws {
+        try await self.request(reauthToken: request.reauthToken)
+    }
+}
