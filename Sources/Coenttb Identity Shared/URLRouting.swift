82b73c74c38d9aca28cb8222fd49985b42ee8444
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-27 16:29:50 +0200

    remove inlinable

--- Sources/Coenttb Identity Shared/URLRouting.swift
+++ Sources/Coenttb Identity Shared/URLRouting.swift
@@ -65,7 +65,6 @@ extension ParserPrinter where Input == URLRequestData {
     ///   - name: Cookie name
     ///   - value: Cookie value
     /// - Returns: Modified BaseURLPrinter with the cookie set
-    @inlinable
     public func cookie(_ name: String, _ value: HTTPCookies.Value) -> BaseURLPrinter<Self> {
         var requestData = URLRequestData()
         requestData.headers["cookie", default: []].append("\(name)=\(value.string)"[...])
@@ -77,7 +76,6 @@ extension ParserPrinter where Input == URLRequestData {
     ///   - name: Cookie name
     ///   - value: Optional cookie value, if nil no cookie is set
     /// - Returns: Modified BaseURLPrinter with the cookie set if value exists
-    @inlinable
     public func cookie(_ name: String, _ value: HTTPCookies.Value?) -> BaseURLPrinter<Self> {
         guard let value = value
         else { return self.baseRequestData(.init()) }
@@ -87,7 +85,6 @@ extension ParserPrinter where Input == URLRequestData {
 //    /// Sets the access token cookie
 //    /// - Parameter token: Optional access token value
 //    /// - Returns: Modified BaseURLPrinter with access_token cookie
-//    @inlinable
 //    public func setAccessToken(_ token: HTTPCookies.Value?) -> BaseURLPrinter<Self> {
 //        return self.cookie("access_token", token)
 //    }
@@ -95,7 +92,6 @@ extension ParserPrinter where Input == URLRequestData {
 //    /// Sets the refresh token cookie
 //    /// - Parameter token: Optional refresh token value
 //    /// - Returns: Modified BaseURLPrinter with refresh_token cookie
-//    @inlinable
 //    public func setRefreshToken(_ token: HTTPCookies.Value?) -> BaseURLPrinter<Self> {
 //        return self.cookie("refresh_token", token)
 //    }
@@ -103,7 +99,6 @@ extension ParserPrinter where Input == URLRequestData {
     /// Sets the reauthorization token header
     /// - Parameter token: Optional reauthorization token value
     /// - Returns: Modified BaseURLPrinter with Reauthorization header
-    @inlinable
     public func setReauthorizationToken(_ token: String?) -> BaseURLPrinter<Self> {
         return transform { urlRequestData in
             if let token = token {
@@ -118,7 +113,6 @@ extension ParserPrinter where Input == URLRequestData {
     /// Sets multiple cookies at once
     /// - Parameter cookies: Dictionary mapping cookie names to values
     /// - Returns: Modified BaseURLPrinter with all cookies set
-    @inlinable
     public func cookies(_ cookies: [String: HTTPCookies.Value]) -> BaseURLPrinter<Self> {
         var requestData = URLRequestData()
         requestData.headers["cookie", default: []].append(
@@ -134,7 +128,6 @@ extension ParserPrinter where Input == URLRequestData {
     /// Transforms the URLRequestData with a provided transformation function
     /// - Parameter transform: Function that takes inout URLRequestData and returns modified URLRequestData
     /// - Returns: Modified BaseURLPrinter
-    @inlinable
     public func transform(_ transform: @escaping (inout URLRequestData) -> URLRequestData) -> BaseURLPrinter<Self> {
         var requestData = URLRequestData()
         requestData = transform(&requestData)
@@ -146,7 +139,6 @@ extension ParserPrinter where Input == URLRequestData {
     /// Sets or removes the Bearer Authorization header
     /// - Parameter token: The bearer token to use for authentication. If nil, no change is made
     /// - Returns: Modified BaseURLPrinter with Authorization header set
-    @inlinable
     public func setBearerAuth(_ token: String?) -> BaseURLPrinter<Self> {
         transform { urlRequestData in
             if let token = token {
