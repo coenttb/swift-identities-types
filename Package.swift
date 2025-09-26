3cb89ce8918294d623133704898fa904acd07285
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-09-27 00:49:00 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ extension Target.Dependency {
 
 extension Target.Dependency {
     static var authentication: Self { .product(name: "Authenticating", package: "swift-authenticating") }
-    static var serverFoundation: Self { .product(name: "ServerFoundation", package: "swift-server-foundation") }
+    static var typesFoundation: Self { .product(name: "TypesFoundation", package: "swift-types-foundation") }
     static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var dependenciesTestSupport: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
 }
@@ -30,14 +30,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/coenttb/swift-authenticating", from: "0.0.1"),
         .package(url: "https://github.com/coenttb/swift-types-foundation", from: "0.0.1"),
-        .package(url: "https://github.com/coenttb/swift-server-foundation", from: "0.0.1"),
+//        .package(url: "https://github.com/coenttb/swift-server-foundation", from: "0.0.1"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2")
     ],
     targets: [
         .target(
             name: .identitiesTypes,
             dependencies: [
-                .serverFoundation,
+                .typesFoundation,
                 .dependenciesMacros,
                 .authentication
             ]
