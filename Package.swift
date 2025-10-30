180523f61de0b978549f4819cec6f3847c463687
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-10-30 15:22:06 +0100

    feat: add swift-jwt dependency to Package.swift
    Add swift-jwt package dependency and import it into the IdentitiesTypes target
    to support JWT token handling in identity operations.

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ extension Target.Dependency {
 
 extension Target.Dependency {
     static var authentication: Self { .product(name: "Authenticating", package: "swift-authenticating") }
+    static var jwt: Self { .product(name: "JWT", package: "swift-jwt") }
     static var typesFoundation: Self { .product(name: "TypesFoundation", package: "swift-types-foundation") }
     static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var dependenciesTestSupport: Self { .product(name: "DependenciesTestSupport", package: "swift-dependencies") }
@@ -30,7 +31,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/coenttb/swift-authenticating", from: "0.0.1"),
         .package(url: "https://github.com/coenttb/swift-types-foundation", from: "0.0.1"),
-//        .package(url: "https://github.com/coenttb/swift-server-foundation", from: "0.0.1"),
+        .package(url: "https://github.com/coenttb/swift-jwt", from: "0.0.1"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2")
     ],
     targets: [
@@ -39,7 +40,8 @@ let package = Package(
             dependencies: [
                 .typesFoundation,
                 .dependenciesMacros,
-                .authentication
+                .authentication,
+                .jwt
             ]
         ),
         .testTarget(
