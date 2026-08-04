2b1fbacb908dff8f5b17b644a8844ae4be4bd776
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-04 03:22:49 +0200

    _Testing: drop redundant -> Void from the typed-throws closure fix

--- Sources/IdentitiesTypes/_Testing/TestHelper.swift
+++ Sources/IdentitiesTypes/_Testing/TestHelper.swift
@@ -20,7 +20,7 @@ extension Identity._TestDatabase {
                 try await withDependencies {
                     $0[Identity._TestDatabase.self] = database
                     $0[Identity.self] = .testValue
-                } operation: { () async throws(Failure) -> Void in
+                } operation: { () async throws(Failure) in
                     try await operation()
                 }
             } else {
